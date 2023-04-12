46b06aeca4d5eb4532d4b1589f237e68d0e925f9
zhanghaoyu <zhanghaoyu@beitai.cn>
2023-04-12 16:04:08 +0800

    实现spring简单的属性注入

--- src/test/java/org/springframework/test/ioc/SimpleBeanContainerTest.java
+++ src/test/java/org/springframework/test/ioc/SimpleBeanContainerTest.java
@@ -2,9 +2,16 @@ package org.springframework.test.ioc;
 
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.PropertyValue;
+import org.springframework.beans.factory.PropertyValues;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanReference;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.test.bean.Car;
+import org.springframework.test.bean.Person;
 import org.springframework.test.service.HelloService;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author: Zhang hao yu
@@ -20,5 +27,28 @@ public class SimpleBeanContainerTest {
 
         HelloService helloService = (HelloService)beanFactory.getBean("helloService");
         helloService.sayHello();
+    }
+
+    @Test
+    public void testPropertyBean(){
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        //注册Car实例
+        PropertyValues propertyValuesForCar = new PropertyValues();
+        propertyValuesForCar.addPropertyValue(new PropertyValue("name", "兰博基尼"));
+        BeanDefinition carBeanDefinition = new BeanDefinition(Car.class, propertyValuesForCar);
+        beanFactory.registerBeanDefinition("car", carBeanDefinition);
+
+        //注册Person实例
+        PropertyValues propertyValuesForPerson = new PropertyValues();
+        propertyValuesForPerson.addPropertyValue(new PropertyValue("name", "张三"));
+        propertyValuesForPerson.addPropertyValue(new PropertyValue("age", 18));
+        //Person实例依赖Car实例
+        propertyValuesForPerson.addPropertyValue(new PropertyValue("car", new BeanReference("car")));
+        BeanDefinition beanDefinition = new BeanDefinition(Person.class, propertyValuesForPerson);
+        beanFactory.registerBeanDefinition("person", beanDefinition);
+
+        Person person = (Person) beanFactory.getBean("person");
+        System.out.println(person);
     }
 }
